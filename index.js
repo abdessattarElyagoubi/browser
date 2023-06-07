8f828f79b70aa426dec27b14d359f03cf7534e72
abdessattarElyagoubi <126204978+abdessattarElyagoubi@users.noreply.github.com>
2023-06-07 19:38:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 app.get('/render', async (req, res) => {
   const url = req.query.url;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0' });
+
+  // Get the base URL of the website
+  const baseURL = new URL(url).origin;
+
+  // Modify the URLs of all resources fetched by Puppeteer to include the base URL
+  await page.$$eval('link[rel="stylesheet"], img, script[src]', (elements, baseURL) => {
+    elements.forEach((element) => {
+      const url = new URL(element.src || element.href, baseURL).href;
+      element.src = url;
+      element.href = url;
+    });
+  }, baseURL);
+
   const content = await page.content();
   await browser.close();
   res.send(content);
