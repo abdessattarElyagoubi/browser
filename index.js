300e1be4c4db2d04500f1b4dfbad9938a2982c78
abdessattarElyagoubi <126204978+abdessattarElyagoubi@users.noreply.github.com>
2023-06-07 20:29:03 +0200

    Update index.js 2

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// Apply CORS to specific URLs while excluding modified URLs
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests from all origins except the modified URLs
+    if (!origin || origin.startsWith('https://browser-o8r6.onrender.com/render?url=')) {
+      callback(null, false);
+    } else {
+      callback(null, true);
+    }
+  },
+  optionsSuccessStatus: 200
+}));
 
 app.get('/render', async (req, res) => {
   const url = req.query.url;
@@ -15,24 +26,34 @@ app.get('/render', async (req, res) => {
   // Get the base URL of the website
   const baseURL = new URL(url).origin;
 
-  // Modify the URLs of all resources fetched by Puppeteer to include the base URL
-  await page.$$eval('link[rel="stylesheet"], img, script[src], a[href]', (elements, baseURL) => {
-    elements.forEach((element) => {
-      let url = element.src || element.href;
-      if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
-        url = new URL(url, baseURL).href;
-        if (element.src) {
-          element.src = url;
-        } else {
-          element.href = url;
-        }
-      }
-    });
-  }, baseURL);
+  // Get the HTML content of the page
+  let html = await page.content();
 
-  const content = await page.content();
+  // Parse the HTML content and modify all URLs and relative URLs to include the base URL
+  const el = new DOMParser().parseFromString(html, 'text/html');
+  const elements = el.querySelectorAll('[src],[href]');
+  elements.forEach((element) => {
+    const url = element.getAttribute('src') || element.getAttribute('href');
+    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
+      if (element.hasAttribute('src')) {
+        element.setAttribute('src', new URL(url, baseURL).href);
+      }
+      if (element.hasAttribute('href')) {
+        element.setAttribute('href', new URL(url, baseURL).href);
+      }
+    }
+  });
+
+  // Get the modified HTML code
+  html = el.documentElement.outerHTML;
+
   await browser.close();
-  res.send(content);
+  res.send(html);
+});
+
+app.post('/data', (req, res) => {
+  // Handle the data
+  res.send('Data received');
 });
 
 app.listen(process.env.PORT || 3000, () => {
